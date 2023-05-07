42ef14fd3eaaf20c7dbaa98f46ec49c76d2cd2c2
anish5256 <anish5256@gmail.com>
2023-05-07 19:06:43 +0530

    chrome driver and requirements.tx

---
+++ driver.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import chromedriver_autoinstaller
+
+class ChromeDriver:
+    def __init__(self):
+        chromedriver_autoinstaller.install()
+        self.driver = webdriver.Chrome()
+
+    def get_driver(self):
+        return self.driver
